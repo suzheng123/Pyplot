7baf50a19a6a26d1c5b9011b2f84715ccecde524
suzheng123 <suzheng@ucsc.edu>
2019-08-18 21:21:50 -0700

    commit

---
+++ prr_record.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+def draw_grid():
+	fig = plt.figure()
+	ax = plt.axes()
+	plt.xlabel("X Axis")
+	plt.ylabel("Y Axis")
+	plt.rc('grid', linestyle="-", color='black')
+	plt.grid(True)
+	return fig
+
+
+def plot_points(fig,d,point_annotation):
+	for coordinate in d:
+		x,y = coordinate
+		point = plt.scatter(x,y)
+		annotation = plt.annotate(d[coordinate],
+					xy = (x,y), xycoords = 'data',
+					xytext=(x+0.2,y),textcoords = 'data',
+					bbox=dict(boxstyle="round", facecolor="#e8ebe9", 
+                  	edgecolor="#a3a8a4")
+			)
+		annotation.set_visible(False)
+		point_annotation.append([point, annotation])       
+
+def hover(event):
+    for point, annotation in point_annotation:
+        show_annotation = (point.contains(event)[0] == True)
+
+        if show_annotation != annotation.get_visible():
+            annotation.set_visible(show_annotation)
+            plt.draw()
+
+
+def coordinate_annotation(d):
+	fig = draw_grid()
+	plot_points(fig,d,point_annotation)
+	connect_id = fig.canvas.mpl_connect('motion_notify_event', hover)
+
+
+if __name__ == '__main__':
+	info_d = {
+		(-5,-4):"PRR Record\nHEAD_NUM: 1\nSITE_NUM:1\nPART_FLG:0x0\nNUM_TEXT:0\nHARD_BIN:1\nSOFT_BIN:1\nX_COORD:-5\nY_COORD:-4\nTEST_T:515\nPART_ID:1\nPART_TXT:0.000\nPART_FIX: ",
+		(-2,-3):"test data"}
+
+	point_annotation = []
+
+	coordinate_annotation(info_d)
+
+	plt.show()
+
+
